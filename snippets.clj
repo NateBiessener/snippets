44101d484c658e4e189603f66f0b2b08e7e03bc3
Nate Biessener <nathaniel.biessener@gmail.com>
2017-01-09 16:37:48 -0600

    interleave two seqs solution

--- snippets.clj
+++ snippets.clj
@@ -1,2 +1,15 @@
 ;takes a start and end number, returns a list of numbers from start(inclusive) to end(exclusive)
-(fn makeRange [start end] (if (< start end) (conj (makeRange (+ start 1) end) start) '() ))
+(defn makeRange [start end] (if (< start end) (conj (makeRange (+ start 1) end) start) '() ))
+
+;Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc.
+(defn ileave [vec1 vec2]
+  (if (or (nil? (first vec1)) (nil? (first vec2)))
+  	nil
+    (let [singlePair [(first vec1) (first vec2)]]
+      (if (and (> (count vec1) 1) (> (count vec2) 1))
+        (apply conj singlePair (ileave (rest vec1) (rest vec2)))
+        singlePair
+      )
+    )
+  )
+)
